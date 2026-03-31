2589ef594bfc9fb88e1d2f2034e79bb4e60a7a93
Gggffdd <165726917+Gggffdd@users.noreply.github.com>
2026-03-31 18:24:04 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -30,7 +31,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
     
     // Networking
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
@@ -38,11 +38,12 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("com.google.code.gson:gson:2.10.1")
     
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:34.11.0"))
+    implementation("com.google.firebase:firebase-auth")
+    
     // Image loading
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    
-    // Bottom navigation
-    implementation("com.google.android.material:material:1.10.0")
     
     // Circle ImageView
     implementation("de.hdodenhof:circleimageview:3.1.0")
