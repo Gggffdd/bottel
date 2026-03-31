a23776312d97bc0f2560f97deac6afd76a6dc2f3
Gggffdd <165726917+Gggffdd@users.noreply.github.com>
2026-03-31 16:43:53 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,16 +30,20 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     
     // Networking
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("com.google.code.gson:gson:2.10.1")
-}
-
-repositories {
-    mavenCentral()
-    google()
-    maven { url = uri("https://jitpack.io") }
+    
+    // Image loading
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    
+    // Bottom navigation
+    implementation("com.google.android.material:material:1.10.0")
+    
+    // Circle ImageView
+    implementation("de.hdodenhof:circleimageview:3.1.0")
 }
