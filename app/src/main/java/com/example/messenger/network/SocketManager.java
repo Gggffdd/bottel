dcf6641d7f50e0667f93d04f9acac03d432639f6
Gggffdd <165726917+Gggffdd@users.noreply.github.com>
2026-03-31 16:06:00 +0300

    Update SocketManager.java

--- app/src/main/java/com/example/messenger/network/SocketManager.java
+++ app/src/main/java/com/example/messenger/network/SocketManager.java
@@ -1,16 +1,12 @@
 package com.example.messenger.network;
 
-import com.github.nkzawa.socketio.client.IO;
-import com.github.nkzawa.socketio.client.Socket;
-import org.json.JSONObject;
-import java.net.URISyntaxException;
-
 public class SocketManager {
     private static SocketManager instance;
-    private Socket socket;
     private OnNewMessageListener messageListener;
 
-    public interface OnNewMessageListener { void onNewMessage(String from, String message, long timestamp); }
+    public interface OnNewMessageListener {
+        void onNewMessage(String from, String message, long timestamp);
+    }
 
     private SocketManager() {}
 
@@ -20,28 +16,18 @@ public class SocketManager {
     }
 
     public void connect(String token) {
-        try {
-            IO.Options opts = new IO.Options();
-            opts.auth = new JSONObject().put("token", token);
-            socket = IO.socket("http://95.85.242.13:3000", opts);
-            socket.on(Socket.EVENT_CONNECT, args -> System.out.println("Connected"));
-            socket.on("new_message", args -> {
-                JSONObject data = (JSONObject) args[0];
-                String from = data.optString("from");
-                String message = data.optString("message");
-                long timestamp = data.optLong("timestamp");
-                if (messageListener != null) messageListener.onNewMessage(from, message, timestamp);
-            });
-            socket.connect();
-        } catch (URISyntaxException e) { e.printStackTrace(); }
+        System.out.println("SocketManager: connect called (stub)");
     }
 
     public void sendMessage(String to, String message) {
-        if (socket != null && socket.connected()) {
-            socket.emit("private_message", new JSONObject().put("to", to).put("message", message));
-        }
+        System.out.println("SocketManager: sendMessage called (stub)");
     }
 
-    public void disconnect() { if (socket != null) socket.disconnect(); }
-    public void setOnNewMessageListener(OnNewMessageListener listener) { this.messageListener = listener; }
+    public void disconnect() {
+        System.out.println("SocketManager: disconnect called (stub)");
+    }
+
+    public void setOnNewMessageListener(OnNewMessageListener listener) {
+        this.messageListener = listener;
+    }
 }
