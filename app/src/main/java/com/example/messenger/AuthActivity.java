7f0bbb0e5b8048d26ab1db3e7b8d8e96db48c265
Gggffdd <165726917+Gggffdd@users.noreply.github.com>
2026-04-01 12:48:33 +0300

    Update AuthActivity.java

--- app/src/main/java/com/example/messenger/AuthActivity.java
+++ app/src/main/java/com/example/messenger/AuthActivity.java
@@ -2,6 +2,7 @@ package com.example.messenger;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.messenger.utils.PreferencesManager;
 
@@ -13,11 +14,21 @@ public class AuthActivity extends AppCompatActivity {
         
         PreferencesManager prefManager = new PreferencesManager(this);
         
-        if (prefManager.isLoggedIn()) {
-            startActivity(new Intent(this, MainActivity.class));
-        } else {
-            startActivity(new Intent(this, EmailVerificationActivity.class));
-        }
-        finish();
+        // Временно показываем статус
+        TextView tv = new TextView(this);
+        tv.setText("AuthActivity\n\nToken: " + prefManager.getToken() + "\n\nПереход через 2 секунды...");
+        tv.setTextSize(16);
+        tv.setPadding(40, 40, 40, 40);
+        setContentView(tv);
+        
+        // Задержка для наглядности
+        new android.os.Handler().postDelayed(() -> {
+            if (prefManager.isLoggedIn()) {
+                startActivity(new Intent(this, MainActivity.class));
+            } else {
+                startActivity(new Intent(this, EmailVerificationActivity.class));
+            }
+            finish();
+        }, 2000);
     }
 }
