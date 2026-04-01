86b24b5b22a56951c09fb4596d3c2a11947b639a
Gggffdd <165726917+Gggffdd@users.noreply.github.com>
2026-04-01 11:20:00 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -1,18 +1,65 @@
 package com.example.messenger;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.example.messenger.fragments.ChatsFragment;
+import com.example.messenger.fragments.ProfileFragment;
+import com.example.messenger.fragments.SettingsFragment;
+import com.example.messenger.utils.PreferencesManager;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private PreferencesManager prefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        prefManager = new PreferencesManager(this);
         
-        TextView tv = new TextView(this);
-        tv.setText("MainActivity работает!");
-        tv.setTextSize(24);
-        setContentView(tv);
+        // Проверяем, залогинен ли пользователь
+        if (!prefManager.isLoggedIn()) {
+            startActivity(new Intent(this, AuthActivity.class));
+            finish();
+            return;
+        }
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        
+        // Загружаем фрагмент чатов по умолчанию
+        loadFragment(new ChatsFragment());
+        
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                
+                if (item.getItemId() == R.id.nav_chats) {
+                    selectedFragment = new ChatsFragment();
+                } else if (item.getItemId() == R.id.nav_profile) {
+                    selectedFragment = new ProfileFragment();
+                } else if (item.getItemId() == R.id.nav_settings) {
+                    selectedFragment = new SettingsFragment();
+                }
+                
+                if (selectedFragment != null) {
+                    loadFragment(selectedFragment);
+                }
+                return true;
+            }
+        });
+    }
+    
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
