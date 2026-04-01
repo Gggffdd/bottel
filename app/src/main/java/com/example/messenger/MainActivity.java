19042edabc5b134c32ab29717f0561e8c84c344e
Gggffdd <165726917+Gggffdd@users.noreply.github.com>
2026-04-01 12:41:39 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.messenger;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import com.example.messenger.fragments.ChatsFragment;
@@ -14,72 +14,50 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-    private PreferencesManager prefManager;
     private BottomNavigationView bottomNavigationView;
+    private PreferencesManager prefManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate started");
-        
+        setContentView(R.layout.activity_main);
+
         prefManager = new PreferencesManager(this);
         
-        // Проверяем токен
-        Log.d(TAG, "Token from prefs: " + prefManager.getToken());
-        Log.d(TAG, "isLoggedIn: " + prefManager.isLoggedIn());
-        
         if (!prefManager.isLoggedIn()) {
-            Log.d(TAG, "User not logged in, redirecting to AuthActivity");
             startActivity(new Intent(this, AuthActivity.class));
             finish();
             return;
         }
-        
-        Log.d(TAG, "User logged in: " + prefManager.getEmail());
-        
-        setContentView(R.layout.activity_main);
-        Log.d(TAG, "setContentView done");
-        
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-        Log.d(TAG, "bottomNavigationView found: " + (bottomNavigationView != null));
         
-        Toast.makeText(this, "Добро пожаловать, " + prefManager.getDisplayName(), Toast.LENGTH_LONG).show();
-        
-        // Загружаем фрагмент чатов по умолчанию
         loadFragment(new ChatsFragment());
         
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            Fragment selectedFragment = null;
-            if (item.getItemId() == R.id.nav_chats) {
-                selectedFragment = new ChatsFragment();
-                Log.d(TAG, "Selected chats");
-            } else if (item.getItemId() == R.id.nav_profile) {
-                selectedFragment = new ProfileFragment();
-                Log.d(TAG, "Selected profile");
-            } else if (item.getItemId() == R.id.nav_settings) {
-                selectedFragment = new SettingsFragment();
-                Log.d(TAG, "Selected settings");
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                
+                if (item.getItemId() == R.id.nav_chats) {
+                    selectedFragment = new ChatsFragment();
+                } else if (item.getItemId() == R.id.nav_profile) {
+                    selectedFragment = new ProfileFragment();
+                } else if (item.getItemId() == R.id.nav_settings) {
+                    selectedFragment = new SettingsFragment();
+                }
+                
+                if (selectedFragment != null) {
+                    loadFragment(selectedFragment);
+                }
+                return true;
             }
-            
-            if (selectedFragment != null) {
-                loadFragment(selectedFragment);
-            }
-            return true;
         });
-        
-        Log.d(TAG, "onCreate finished");
     }
     
     private void loadFragment(Fragment fragment) {
-        try {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, fragment)
-                    .commit();
-            Log.d(TAG, "Fragment loaded: " + fragment.getClass().getSimpleName());
-        } catch (Exception e) {
-            Log.e(TAG, "Error loading fragment", e);
-            Toast.makeText(this, "Ошибка загрузки: " + e.getMessage(), Toast.LENGTH_LONG).show();
-        }
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
